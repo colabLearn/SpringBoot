117dc1167ee7c2e7d574774ace61b60a667356a0
colabLearn <tunji4physics@gmail.com>
2022-08-21 17:33:08 -0400

    Successfully add delete and edit actions we implement delete action

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -5,14 +5,15 @@ import React from 'react'
 
 import './App.css';
 import {useState, useEffect} from 'react';
-import {getAllStudents} from "./client";
+import {deleteStudent, getAllStudents} from "./client";
 import {
     Avatar,
     Badge,
     Breadcrumb, Button, Empty,
     Layout,
     Menu, Spin,
-    Table, Tag
+    Table, Tag,
+    Radio
 } from 'antd';
 
 import {
@@ -23,7 +24,9 @@ import {
     TeamOutlined,
     UserOutlined,
 } from '@ant-design/icons';
+import { message, Popconfirm } from 'antd';
 import StudentDrawerForm from "./StudentDrawerForm";
+import {successNotification} from "./Notification";
 const { Header, Content, Footer, Sider } = Layout;
 const {SubMenu} =Menu;
 const TheAvatar = ({name}) => {
@@ -39,10 +42,17 @@ const TheAvatar = ({name}) => {
         {`${name.charAt(0)}${name.charAt(name.length-1)}`}
     </Avatar>
 }
+const removeStudent = (studentId, callback) => {
+    deleteStudent(studentId).then(() => {
+        successNotification("Student deleted",  `Student with id ${studentId} is deleted`)
+        callback();
+    })
+};
 
-const columns = [
+
+const columns = fetchStudents => [
     {
-        title: '',
+        title: 'Avatar',
         dataIndex: 'avatar',
         key: 'avatar',
         render: (text, student) => <TheAvatar name={student.name}/>
@@ -67,6 +77,22 @@ const columns = [
         title: 'Gender',
         dataIndex: 'gender',
         key: 'gender',
+    },
+    {
+        title: 'Action',
+        key: 'action',
+        render:(text, student)=>
+        <Radio.Group>
+            <Popconfirm
+                placement='topRight'
+                title= {`Are you sure to delete ${student.name}?`}
+                onConfirm={() => removeStudent(student.id, fetchStudents)}
+                okText="Yes"
+                cancelText="No" >
+                <Radio.Button value="small">Delete</Radio.Button>
+             </Popconfirm>
+            <Radio.Button value="small">Edit</Radio.Button>
+        </Radio.Group>
     },
 ];
 
@@ -108,7 +134,7 @@ function App() {
             />
             <Table
             dataSource={students}
-            columns={columns}
+            columns={columns(fetchStudents)}
             bordered
             title={() =>
                 <>
--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -3,7 +3,6 @@ package com.example.demo.student;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,20 +10,22 @@ import java.util.List;
 @RequestMapping(path = "api/v1/students")
 @AllArgsConstructor
 public class StudentController {
-    private final StudentService studentService;
-   //Let's create some API endpoints
-    //This would return a list of students
 
-    //To explose this as an endpoint we anotate with @GetMapping
+    private final StudentService studentService;
+
     @GetMapping
     public List<Student> getAllStudents() {
-        //Extract with variables --- Ctrl+Alt+Shift
-
         return studentService.getAllStudents();
     }
 
     @PostMapping
     public void addStudent(@RequestBody Student student) {
         studentService.addStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(
+            @PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
     }
 }
