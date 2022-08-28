141d98adbb7d7b8232cff32a6a1fd44fe038adc1
colabLearn <tunji4physics@gmail.com>
2022-08-28 16:32:07 -0400

    added noopener noreferrer

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -233,6 +233,7 @@ function App() {
                 />
                 <Divider>
                     <a
+                        rel="noopener noreferrer"
                         target="_blank"
                         href="https://amigoscode.com/">
                         Click here to access Fullstack Spring Boot & React Training from Amigoscode</a>
