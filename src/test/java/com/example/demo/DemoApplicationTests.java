1cfa3ff151ee3325ae7235fb18ed14354f591885
colabLearn <tunji4physics@gmail.com>
2022-09-01 15:48:35 -0400

    Merge remote-tracking branch 'main/add_footer' into add_footer

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,30 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
 class DemoApplicationTests {
 
+    Calculator underTest = new Calculator();
+
     @Test
-    void contextLoads() {
+    void itShouldAddTwoNumbers() {
+        //given
+        int numberOne = 15;
+        int numberTwo = 35;
+
+        // when
+        int result = underTest.add(numberOne, numberTwo);
+
+        //then
+        assertThat(result).isEqualTo(50);
+    }
+
+    class Calculator {
+        int add(int a, int b) {
+            return a + b;
+        }
     }
 
 }
